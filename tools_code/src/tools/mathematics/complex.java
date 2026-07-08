e4c517a200456446fc1652f19b7de7eab27eb031
初夏 <z1085299491@163.com>
2026-07-08 11:46:36 +0800

    重命名了complex_number→complex类

--- tools_code/src/tools/mathematics/complex.java
+++ tools_code/src/tools/mathematics/complex.java
@@ -2,26 +2,27 @@ package tools.mathematics;
 /**
 <p>复数类。</p><br>
 复数是由实部和虚部组成的数。<br>
-形式为a+bi，其中a为实部，b为虚部，i为虚数单位。
+形式为a+bi，其中a为实部，b为虚部，i为虚数单位。<br>
+当a=0时，为纯虚数对象；当b=0时，为实数对象。
 */
-public class complex_number
+public class complex
 {
     public double real;
     public double imaginary;
     /**
-    创建一个新的复数对象 0。
+    构造一个复数对象0。
     */
-    public complex_number()
+    public complex()
     {
         this.real=0;
         this.imaginary=0;
     }
     /**
-    创建一个新的复数对象 <code>real</code>+<code>imaginary</code>*i。
+    构造一个复数对象 <code>real</code>+<code>imaginary</code>*i。
     @param real 实部。
     @param imaginary 虚部。
     */
-    public complex_number(double real,double imaginary)
+    public complex(double real,double imaginary)
     {
         this.real=real;
         this.imaginary=imaginary;
@@ -32,7 +33,7 @@ public class complex_number
     @param C 要相加的复数对象。
     @return 和是否为实数。
     */
-    public boolean add(complex_number C)
+    public boolean add(complex C)
     {
         this.real+=C.real;
         this.imaginary+=C.imaginary;
@@ -44,9 +45,9 @@ public class complex_number
     @param C2 第二个复数对象。
     @return 两个复数对象的和。
     */
-    public static complex_number add(complex_number C1,complex_number C2)
+    public static complex add(complex C1,complex C2)
     {
-        return new complex_number(C1.real+C2.real,C1.imaginary+C2.imaginary);
+        return new complex(C1.real+C2.real,C1.imaginary+C2.imaginary);
     }
     /**
 	<p>此方法会修改调用对象。</p><br>
@@ -54,7 +55,7 @@ public class complex_number
     @param C 要相减的复数对象。
     @return 差是否为实数。
     */
-    public boolean subtract(complex_number C)
+    public boolean subtract(complex C)
     {
         this.real-=C.real;
         this.imaginary-=C.imaginary;
@@ -66,9 +67,9 @@ public class complex_number
     @param C2 第二个复数对象。
     @return 两个复数对象的差。
     */
-    public static complex_number subtract(complex_number C1,complex_number C2)
+    public static complex subtract(complex C1,complex C2)
     {
-        return new complex_number(C1.real-C2.real,C1.imaginary-C2.imaginary);
+        return new complex(C1.real-C2.real,C1.imaginary-C2.imaginary);
     }
     /**
 	<p>此方法会修改调用对象。</p><br>
@@ -76,7 +77,7 @@ public class complex_number
     @param C 要相乘的复数对象。
     @return 积是否为实数。
     */
-    public boolean multiply(complex_number C)
+    public boolean multiply(complex C)
     {
         double real=this.real*C.real-this.imaginary*C.imaginary;
         double imaginary=this.real*C.imaginary+this.imaginary*C.real;
@@ -90,9 +91,9 @@ public class complex_number
     @param C2 第二个复数对象。
     @return 两个复数对象的积。
     */
-    public static complex_number multiply(complex_number C1,complex_number C2)
+    public static complex multiply(complex C1,complex C2)
     {
-        return new complex_number(C1.real*C2.real-C1.imaginary*C2.imaginary,C1.real*C2.imaginary+C1.imaginary*C2.real);
+        return new complex(C1.real*C2.real-C1.imaginary*C2.imaginary,C1.real*C2.imaginary+C1.imaginary*C2.real);
     }
     /**
 	<p>此方法会修改调用对象。</p><br>
@@ -100,7 +101,7 @@ public class complex_number
     @param C 要相除的复数对象。
     @return 商是否为实数。
     */
-    public boolean divide(complex_number C)
+    public boolean divide(complex C)
     {
         double real=(this.real*C.real+this.imaginary*C.imaginary)/(C.real*C.real+C.imaginary*C.imaginary);
         double imaginary=(this.imaginary*C.real-this.real*C.imaginary)/(C.real*C.real+C.imaginary*C.imaginary);
@@ -114,9 +115,9 @@ public class complex_number
     @param C2 第二个复数对象。
     @return 两个复数对象的商。
     */
-    public static complex_number divide(complex_number C1,complex_number C2)
+    public static complex divide(complex C1,complex C2)
     {
-        return new complex_number((C1.real*C2.real+C1.imaginary*C2.imaginary)/(C2.real*C2.real+C2.imaginary*C2.imaginary),(C1.imaginary*C2.real-C1.real*C2.imaginary)/(C2.real*C2.real+C2.imaginary*C2.imaginary));
+        return new complex((C1.real*C2.real+C1.imaginary*C2.imaginary)/(C2.real*C2.real+C2.imaginary*C2.imaginary),(C1.imaginary*C2.real-C1.real*C2.imaginary)/(C2.real*C2.real+C2.imaginary*C2.imaginary));
     }
     /**
     计算当前复数对象的模长。
