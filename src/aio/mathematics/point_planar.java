628bf9433223e972c429c8c9b3aeefde93665dad
初夏 <z1085299491@163.com>
2026-08-25 20:27:18 +0800

    将直角坐标类重命名为平面点类

--- src/aio/mathematics/point_planar.java
+++ src/aio/mathematics/point_planar.java
@@ -1,11 +1,10 @@
 package aio.mathematics;
 /**
-<p>直角坐标类</p><br>
-直角坐标表示平面直角坐标系中的一个位置。<br>
-横坐标和纵坐标都是实数，取值范围为任意实数。<br>
-@see aio.geography.projected_coordinate
+<p>平面点类</p><br>
+平面点表示平面中的一个位置。<br>
+横坐标和纵坐标都是实数，取值范围为任意实数。
 */
-public class coordinate_cartesian
+public class point_planar
 {
     /**
     <p>横坐标</p>
@@ -17,28 +16,28 @@ public class coordinate_cartesian
     public double y;
     /**
     <p>全参构造方法</p><br>
-    构造一个直角坐标对象。
+    构造一个平面点对象。
     @param x 横坐标。
     @param y 纵坐标。
     */
-    public coordinate_cartesian(double x,double y)
+    public point_planar(double x,double y)
     {
         this.x=x;
         this.y=y;
     }
     /**
     <p>无参构造方法</p><br>
-    构造一个默认的直角坐标对象，表示原点。
+    构造一个默认的平面点对象，表示原点。
     */
-    public coordinate_cartesian()
+    public point_planar()
     {
         this.x=0;
         this.y=0;
     }
     /**
     <p>点象限计算</p><br>
-    计算当前直角坐标所在的象限。
-    @return 当前直角坐标对象所在的象限。
+    计算当前平面点所在的象限。
+    @return 当前平面点所在的象限。
     */
     public int quadrant()
     {
@@ -46,10 +45,10 @@ public class coordinate_cartesian
     }
     /**
     <p>点象限计算</p><br>
-    计算指定直角坐标所在的象限。
+    计算指定平面点所在的象限。
     @param x 横坐标。
     @param y 纵坐标。
-    @return 直角坐标(<code>x</code>,<code>y</code>)所在的象限。
+    @return 平面点(<code>x</code>,<code>y</code>)所在的象限。
     */
     public static int quadrant(double x,double y)
     {
@@ -58,51 +57,51 @@ public class coordinate_cartesian
     /**
     <p>坐标相加</p><br>
     <p>此方法会修改调用对象。</p><br>
-    将当前直角坐标与另一个直角坐标相加。
-    @param coordinate_cartesian 要相加的直角坐标对象。
+    将当前平面点与另一个平面点相加。
+    @param coordinate_cartesian 要相加的平面点对象。
     */
-    public void add(coordinate_cartesian coordinate_cartesian)
+    public void add(point_planar coordinate_cartesian)
     {
         x+=coordinate_cartesian.x;
         y+=coordinate_cartesian.y;
     }
     /**
     <p>坐标相加</p><br>
-    计算两个直角坐标的和 <code>coordinate1</code>+<code>coordinate2</code>。
-    @param coordinate1 第一个直角坐标对象。
-    @param coordinate2 第二个直角坐标对象。
-    @return 两个直角坐标的和。
+    计算两个平面点的和 <code>coordinate1</code>+<code>coordinate2</code>。
+    @param coordinate1 第一个平面点对象。
+    @param coordinate2 第二个平面点对象。
+    @return 两个平面点的和。
     */
-    public static coordinate_cartesian add(coordinate_cartesian coordinate1,coordinate_cartesian coordinate2)
+    public static point_planar add(point_planar coordinate1,point_planar coordinate2)
     {
-        return new coordinate_cartesian(coordinate1.x+coordinate2.x,coordinate1.y+coordinate2.y);
+        return new point_planar(coordinate1.x+coordinate2.x,coordinate1.y+coordinate2.y);
     }
     /**
     <p>坐标相减</p><br>
     <p>此方法会修改调用对象。</p><br>
-    将当前直角坐标与另一个直角坐标相减。
-    @param coordinate_cartesian 要相减的直角坐标对象。
+    将当前平面点与另一个平面点相减。
+    @param coordinate_cartesian 要相减的平面点对象。
     */
-    public void subtract(coordinate_cartesian coordinate_cartesian)
+    public void subtract(point_planar coordinate_cartesian)
     {
         x-=coordinate_cartesian.x;
         y-=coordinate_cartesian.y;
     }
     /**
     <p>坐标相减</p><br>
-    计算两个直角坐标的差 <code>coordinate1</code>-<code>coordinate2</code>。
-    @param coordinate1 第一个直角坐标对象。
-    @param coordinate2 第二个直角坐标对象。
-    @return 两个直角坐标的差。
+    计算两个平面点的差 <code>coordinate1</code>-<code>coordinate2</code>。
+    @param coordinate1 第一个平面点对象。
+    @param coordinate2 第二个平面点对象。
+    @return 两个平面点的差。
     */
-    public static coordinate_cartesian subtract(coordinate_cartesian coordinate1,coordinate_cartesian coordinate2)
+    public static point_planar subtract(point_planar coordinate1,point_planar coordinate2)
     {
-        return new coordinate_cartesian(coordinate1.x-coordinate2.x,coordinate1.y-coordinate2.y);
+        return new point_planar(coordinate1.x-coordinate2.x,coordinate1.y-coordinate2.y);
     }
     /**
     <p>坐标数乘</p><br>
     <p>此方法会修改调用对象。</p><br>
-    计算当前直角坐标与一个系数的乘积。
+    计算当前平面点与一个系数的乘积。
     @param coefficient 系数。
     */
     public void multiply_scalar(double coefficient)
@@ -112,22 +111,22 @@ public class coordinate_cartesian
     }
     /**
     <p>坐标数乘</p><br>
-    计算一个直角坐标与一个系数的乘积 <code>coordinate_cartesian</code>*<code>coefficient</code>。
-    @param coordinate_cartesian 要数乘的直角坐标对象。
+    计算一个平面点与一个系数的乘积 <code>coordinate_cartesian</code>*<code>coefficient</code>。
+    @param coordinate_cartesian 要数乘的平面点对象。
     @param coefficient 系数。
-    @return 一个直角坐标与一个系数的乘积。
+    @return 一个平面点与一个系数的乘积。
     */
-    public static coordinate_cartesian multiply_scalar(coordinate_cartesian coordinate_cartesian,double coefficient)
+    public static point_planar multiply_scalar(point_planar coordinate_cartesian,double coefficient)
     {
-        return new coordinate_cartesian(coordinate_cartesian.x*coefficient,coordinate_cartesian.y*coefficient);
+        return new point_planar(coordinate_cartesian.x*coefficient,coordinate_cartesian.y*coefficient);
     }
     /**
     <p>两点距离</p><br>
-    计算当前直角坐标与另一个直角坐标的距离。
-    @param coordinate_cartesian 要计算距离的直角坐标对象。
-    @return 当前直角坐标与另一个直角坐标的距离。
+    计算当前平面点与另一个平面点的距离。
+    @param coordinate_cartesian 要计算距离的平面点对象。
+    @return 当前平面点与另一个平面点的距离。
     */
-    public double distance(coordinate_cartesian coordinate_cartesian)
+    public double distance(point_planar coordinate_cartesian)
     {
         return Math.sqrt((x-coordinate_cartesian.x)*(x-coordinate_cartesian.x)+(y-coordinate_cartesian.y)*(y-coordinate_cartesian.y));
     }
@@ -175,10 +174,10 @@ public class coordinate_cartesian
     /**
     <p>相对角度</p><br>
     计算该点到终点的角度。
-    @param target 终点的直角坐标对象。
+    @param target 终点的平面点对象。
     @return 该点到终点的角度。
     */
-    public double angle(coordinate_cartesian target)
+    public double angle(point_planar target)
     {
         double dx=target.x-x;
         double dy=target.y-y;
@@ -231,37 +230,37 @@ public class coordinate_cartesian
     }
     /**
     <p>中点</p><br>
-    计算该直角坐标与指定直角坐标连线的中点。
-    @param target 目标直角坐标对象。
-    @return 该直角坐标与指定直角坐标连线的中点。
+    计算该平面点与指定平面点连线的中点。
+    @param target 目标平面点对象。
+    @return 该平面点与指定平面点连线的中点。
     */
-    public coordinate_cartesian middle_point(coordinate_cartesian target)
+    public point_planar middle_point(point_planar target)
     {
-        return new coordinate_cartesian((target.x+x)/2,(target.y+y)/2);
+        return new point_planar((target.x+x)/2,(target.y+y)/2);
     }
     /**
     <p>线性插值</p><br>
-    计算该直角坐标到指定直角坐标的线性插值。
-    @param target 目标直角坐标对象。
+    计算该平面点到指定平面点的线性插值。
+    @param target 目标平面点对象。
     @param ratio 线性插值比例。<br>
     <code>ratio</code>∈[0,1]，0表示起点，1表示目标点。
-    @return 该直角坐标到指定直角坐标的线性插值。
+    @return 该平面点到指定平面点的线性插值。
     */
-    public coordinate_cartesian linear_interpolation(coordinate_cartesian target,double ratio)
+    public point_planar linear_interpolation(point_planar target,double ratio)
     {
-        return new coordinate_cartesian(x+ratio*(target.x-x),y+ratio*(target.y-y));
+        return new point_planar(x+ratio*(target.x-x),y+ratio*(target.y-y));
     }
     /**
     <p>向目标点位移距离</p><br>
     <p>此方法会修改调用对象。</p><br>
-    将当前直角坐标向指定直角坐标位移指定距离。<br>
+    将当前平面点向指定平面点位移指定距离。<br>
     注意，坐标经计算后可能存在双精度浮点数精度误差，导致位移结果不准确。
-    @param target 目标直角坐标对象。
+    @param target 目标平面点对象。
     @param move_distance 距离。
     @return 位移的距离占总距离的比例。<br>
     0表示起点，1表示目标点。
     */
-    public double move_distance_towards(coordinate_cartesian target,double move_distance)
+    public double move_distance_towards(point_planar target,double move_distance)
     {
         double dx=target.x-x;
         double dy=target.y-y;
@@ -278,13 +277,13 @@ public class coordinate_cartesian
     /**
     <p>向目标点位移比例</p><br>
     <p>此方法会修改调用对象。</p><br>
-    将当前直角坐标向指定直角坐标位移指定比例。
-    @param target 目标直角坐标对象。
+    将当前平面点向指定平面点位移指定比例。
+    @param target 目标平面点对象。
     @param move_ratio 比例。<br>
     <code>move_ratio</code>∈[0,1]，0表示起点，1表示目标点。
     @return 位移的距离。
     */
-    public double move_ratio_towards(coordinate_cartesian target,double move_ratio)
+    public double move_ratio_towards(point_planar target,double move_ratio)
     {
         double dx=target.x-x;
         double dy=target.y-y;
@@ -299,7 +298,7 @@ public class coordinate_cartesian
     @return 多边形的周长。<br>
     若多边形的顶点不足2个，则返回0.0。
     */
-    public static double perimeter(coordinate_cartesian... coordinates_ccw)
+    public static double perimeter(point_planar... coordinates_ccw)
     {
         if(coordinates_ccw.length>=2)
         {
@@ -329,7 +328,7 @@ public class coordinate_cartesian
     @return 多边形的面积。<br>
     若多边形的顶点不足3个，则返回0.0。
     */
-    public static double area(coordinate_cartesian... coordinates_ccw)
+    public static double area(point_planar... coordinates_ccw)
     {
         if(coordinates_ccw.length>=3)
         {
@@ -361,7 +360,7 @@ public class coordinate_cartesian
     }
     /**
     <p>字符串表示</p><br>
-    @return 直角坐标的字符串表示。
+    @return 平面点的字符串表示。
     */
     public String toString()
     {
