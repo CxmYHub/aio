9977a179a2b6711aab288f130c1942ef5dc704ee
初夏 <z1085299491@163.com>
2026-08-25 20:40:21 +0800

    添加了对aio包的描述文档。

---
+++ src/aio/package-info.java
@@ -0,0 +1,13 @@
+/**
+<p>AIO</p><br>
+工具包根包，包含以下子包：<br>
+<ul>
+    <li>collection：集合工具包。</li>
+    <li>data_structure：数据结构工具包。</li>
+    <li>date_time：日期时间工具包。</li>
+    <li>encode_decode：编码解码工具包。</li>
+    <li>geography：地理工具包。</li>
+    <li>mathematics：数学工具包。</li>
+</ul>
+*/
+package aio;
