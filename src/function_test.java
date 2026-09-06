47a13928e0a646f998b059a4c9b55194bedb0e2e
初夏 <z1085299491@163.com>
2026-09-06 17:58:21 +0800

    新增了一元实函数类的测试类。

---
+++ src/function_test.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import aio.mathematics.*;
+public class function_test
+{
+    public static Scanner s1=new Scanner(System.in);
+    public static Random RNG=new Random();
+    public static void main(String args[])
+    {
+        double x[]={1641,2192,2436,2919,3253,3470,3569,3834};
+        double y[]={223,366,440,592,689,745,768,823};
+        // double x[]={0,1,2,3,4};
+        // double y[]={0,1,4,9,16};
+        long start=System.currentTimeMillis();
+        function f=new function(x,y);
+        long end=System.currentTimeMillis();
+        System.out.println(end-start);
+        System.out.println(f);
+        System.out.println(f.fitness);
+        System.out.println(Arrays.toString(x));
+        System.out.println(Arrays.toString(y));
+        System.out.println(Arrays.toString(f.calculate(x)));
+        // double xy[]=new double[x.length<<1];
+        // for(int i=0;i<x.length;i++)
+        // {
+        //     xy[i<<1]=x[i];
+        //     xy[i<<1|1]=y[i];
+        // }
+        // System.out.println(Arrays.toString(xy));
+        // function g=new function(xy);
+        // System.out.println(g);
+    }
+}
